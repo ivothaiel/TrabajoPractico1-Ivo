6041e5632f449052f21e90cb1e8d5a477f9ace60
admin <admin@192.168.1.11>
2024-04-10 13:22:42 -0300

    ejercicio 11 terminado y trabajo practico terminado

--- tp1_VicencioIvo/src/ar/edu/unju/fi/ejercicio11/Main.java
+++ tp1_VicencioIvo/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -3,12 +3,11 @@ package ar.edu.unju.fi.ejercicio11;
 public class Main {
 
 	public static void main(String[] args) {
-		int j = 40;
-        int i = 2;
-        
-        for (; j >= 0; i++, j -= 2) {
-            int resultado = i * j;
-            System.out.print(resultado + " ");
+	     int i,j, numero = 80;
+        for (i=2, j = 40;j>0;i++,j--) {
+            
+            System.out.print(numero + " ");
+            numero+=(j-i-1);
         }
 	}
 
